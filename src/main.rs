18edf3986338c555310f2d86601720c57a71b613
Glendon Solsberry <gms8994@gmail.com>
2020-05-05 20:24:32 -0400

    This feels a little better at least

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,60 @@
-//!
-#![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
+use tokio;
+use boinc_rpc::{models};
 
 fn main() {
+    println!("program started");
+
+    let mut hosts = Vec::new();
+
+    let host = Host::new("127.0.0.1:31416", Some("1033644eaad1ea7d91bc48a749f1620b"));
+    hosts.push(host);
+
     tokio::runtime::Runtime::new().unwrap().block_on(async {
-        let transport = boinc_rpc::Transport::new("127.0.0.1:31416", Some("77c858a8407c88f53d261e4849967cdd"));
+        fetch_from_hosts(&mut hosts).await;
+    });
+
+    dbg!(&hosts);
+
+    println!("program finished");
+}
+
+#[derive(Debug)]
+pub struct Host {
+    addr: &'static str,
+    password: Option<&'static str>,
+    projects: Option<Vec<models::ProjectInfo>>,
+    results: Option<Vec<models::TaskResult>>,
+}
+
+impl Host {
+    pub fn new(addr : &'static str, password : Option<&'static str>) -> Self {
+        Host {
+            addr : addr,
+            password : password,
+            projects : None,
+            results : None,
+        }
+    }
+}
+
+async fn fetch_from_hosts(hosts : &mut Vec<Host>)
+{
+    for (idx, host) in hosts.into_iter().enumerate() {
+        let transport = boinc_rpc::Transport::new(host.addr, host.password);
         let mut client = boinc_rpc::Client::new(transport);
 
         let projects = match client.get_projects().await {
-            Ok(t) => t,
-            Err(t) => panic!("Can't connect to 127.0.0.1:31416"),
+            Ok(t) => Some(t),
+            Err(t) => panic!(t),
         };
+
+        host.projects = projects;
 
         let results = match client.get_results(false).await {
-            Ok(t) => t,
-            Err(t) => panic!("Can't connect to 127.0.0.1:31416"),
+            Ok(t) => Some(t),
+            Err(t) => panic!(t),
         };
 
-        for i in 0..projects.len() {
-            let project = &projects[i];
-
-            println!("{:?}\n", project);
-        }
-
-        for i in 0..results.len() {
-            let result = &results[i];
-
-            println!("{:?}\n", result);
-        }
-    })
+        host.results = results;
+    }
 }
